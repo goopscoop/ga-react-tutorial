6e60b2120db2dd51e80f16fd8649745a82ef152a
Cody <cody.barrus@concur.com>
2015-12-31 16:12:21 -0800

    setup work complete

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render(){
+    return(
+      <div>Hello World</div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');  
+module.exports = {  
+    entry: [
+      'webpack/hot/only-dev-server',
+      "./src/app.js"
+    ],
+    output: {
+        path: __dirname + '/build',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { 
+              test: /\.js?$/, 
+              loaders: ['react-hot', 'babel'],
+              exclude: /node_modules/
+              // query: {
+              //   presets: ['react']
+              // }
+            },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    },
+    plugins: [
+      new webpack.NoErrorsPlugin()
+    ]
+};
+
