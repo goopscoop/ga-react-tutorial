92563064fc102e089866bdb0e62da1cb09c7afaf
goopscoop <codybarrus@gmail.com>
2016-02-08 20:54:10 -0800

    changed npm start

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
 
 var webpack = require('webpack');
 module.exports = {
-    entry: [
-      'babel-polyfill',
-      'webpack/hot/only-dev-server',
-      './src/app.js'
-    ],
-    output: {
-        path: __dirname + '/build',
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              loader: 'babel-loader',
-              query: {
-                plugins: ['transform-runtime'],
-                presets:['es2015', 'react', 'stage-0']
-              }
-            },
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    }
+  entry: [
+    'babel-polyfill',
+    // 'webpack/hot/only-dev-server',
+    './src/app.js'
+  ],
+  output: {
+    path: __dirname + '/build',
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          plugins: ['transform-runtime'],
+          presets:['es2015', 'react', 'stage-0']
+        }
+      },
+      { test: /\.css$/, loader: "style!css" }
+    ]
+  }
 };
